97b2ae718954b72c098516251e77af6bf6cdf3f5
Deepti-web <deeptinirmalyamohanty@gmail.com>
2026-07-16 21:59:31 +0530

    16-07-2026 || 21:59

--- app/services/retrieval/__init__.py
+++ app/services/retrieval/__init__.py
@@ -1,3 +1,3 @@
-from app.services.retrival.embedding import embed_query, embed_texts, get_embedding_dim
+from app.services.retrieval.embedding import embed_query, embed_texts, get_embedding_dim
 
 __all__ = ["embed_query", "embed_texts", "get_embedding_dim"]
